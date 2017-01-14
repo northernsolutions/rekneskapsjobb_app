b6ef1ec176e6cf078869e022ae638e563e045ec2
Jacob Hedengren <=>
2017-01-14 14:55:57 +0100

    added for_employers controller with index actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :for_employers
+
   root 'coming_soon#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
